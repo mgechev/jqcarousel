e91056cc8dbf47cf15be31653ef8ac00bca28db9
Minko Gechev <mgechev@gmail.com>
2013-03-30 14:20:12 +0200

    Better formatting

--- src/jqcarousel.js
+++ src/jqcarousel.js
@@ -13,8 +13,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+                      jslint nomen: true
+
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-/*jslint nomen: true */
 
 ;(function ($) {
 
